7b7df80d2db25b48c4596b50d889dc602af5a256
ScrapWareOrg <github@twinkle.work>
2025-07-31 01:39:56 +0900

    0.52.0

---
+++ src/twlog/Handlers/ChatGPT/SysLog.py
@@ -0,0 +1,36 @@
+#!/home/twinkle/venv/bin/python
+
+import socket
+
+######################################################################
+# LIBS
+
+from twlog.util.Code import *
+from twlog.Formatters import Formatter, LogRecord
+from twlog.Handlers import Handler
+
+######################################################################
+# Classes - ChatGPT
+
+class SyslogHandler(Handler):
+    def __init__(self, address=("localhost", 514), level=INFO):
+        super().__init__(level)
+        self.address = address
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def emit(self, record):
+        message = self.format(record)
+        self.socket.sendto(message.encode("utf-8"), self.address)
+
+######################################################################
+# MAIN
+if __name__ == "__main__":
+    print(f"[{__name__}]")
+    print(__doc__)
+
+#=====================================================================
+# ALL - Make it directly accessible from the top level of the package
+__all__ = ["SyslogHandler"]
+
+""" __DATA__
+
+__END__ """
